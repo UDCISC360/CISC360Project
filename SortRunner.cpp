97ed7c115c54394aaf5cfd82c89aa7da00267d20
erickurtz <erickurtz@live.com>
2017-12-02 13:50:27 -0500

    Copied SamLocal to sortRunner.

--- SortRunner.cpp
+++ SortRunner.cpp
@@ -1,14 +1,17 @@
-//Author Eric
+//Author Eric, Sam
 #include <cstdlib>
 #include <ctime>
 #include <utility>
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <stdio.h>
+#include<omp.h>
+#include <stdlib.h>
+#include <string.h>
 
 int ITERATE = 10; //number of iterations for each test. 
-int LEN_CONST [7] = {1, 5, 10, 100, 1000, 10000, 100000};
+int LEN_CONST [7] = {100, 200, 500, 1000, 5000, 10000, 15000};
 int LEN_CONST_LEN = 7;
 int MAXVAL = 10000;
 int NUMTHREADS = 4; 
@@ -41,8 +44,15 @@ void insertionsort(int a[], int n, int stride)
         a[i+stride] = key;
     }
 }
-
-
+int newgap(int gap)
+{
+    gap = (gap * 10) / 13;
+    if (gap == 9 || gap == 10)
+        gap = 11;
+    if (gap < 1)
+        gap = 1;
+    return gap;
+}
 
 int * generateRandomArray(int len){
 
@@ -71,24 +81,26 @@ int * generateBackwardsArray(int len){
   
 
 void sortRun(int* lenArrs, int lenArrsLen,
-	     void (*sortFun)(int*, int), int* (*genFun)(int) ){
-  duration = 0;
-  durationAvg = 0; 
-    for(int i = 0; i < lenArrsLen; i++){
-       for(int k = 0; k < ITERATE; k++){
-	 //loop for each Len in LEN_CONST
-	 //create a new array, populate array
-	 int* tbs = genFun(lenArrs[i]); 
-	 // print_array(lenArrs[i],tbs); 
-	 start = clock(); 
-	 sortFun(tbs, lenArrs[i]);
-	 end = clock(); 
-	 durationAvg += ( end - start ) / (double) CLOCKS_PER_SEC;
-	 free(tbs);
-      //print_array(lenArrs[i],tbs);
-       }
-       duration = durationAvg/ITERATE;
-       std::cout<<"time: "<< duration<<" length: " << lenArrs[i]<<'\n';
+	void (*sortFun)(int*, int), int* (*genFun)(int) ){
+	duration = 0;
+	durationAvg = 0; 
+    for(int i = 0; i < lenArrsLen; i++)
+	{
+		for(int k = 0; k < ITERATE; k++)
+		{
+			//loop for each Len in LEN_CONST
+			//create a new array, populate array
+			int* tbs = genFun(lenArrs[i]); 
+			// print_array(lenArrs[i],tbs); 
+			start = clock(); 
+			sortFun(tbs, lenArrs[i]);
+			end = clock(); 
+			durationAvg += ( end - start ) / (double) CLOCKS_PER_SEC;
+			free(tbs);
+			//print_array(lenArrs[i],tbs);
+        }
+		duration = durationAvg/ITERATE;
+		std::cout<<"time: "<< duration<<" length: " << lenArrs[i]<<'\n';
     }
     
 }
@@ -109,29 +121,18 @@ void swap(int *xp, int *yp)
 //http://www.geeksforgeeks.org/bubble-sort/
 void bubbleSort(int arr[], int n)
 {
-   int i, j;
-   for (i = 0; i < n-1; i++)      
+	int i, j;
+	for (i = 0; i < n-1; i++)      
  
-       // Last i elements are already in place   
-       for (j = 0; j < n-i-1; j++) 
-           if (arr[j] > arr[j+1])
-	     swap(&arr[j], &arr[j+1]);
+    // Last i elements are already in place   
+		for (j = 0; j < n-i-1; j++) 
+			if (arr[j] > arr[j+1])
+				swap(&arr[j], &arr[j+1]);
 }
 
 
 //https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Comb_sort
 // Function to sort a[0..n-1] using Comb Sort
-
-// To find gap between elements
-int getNextGap(int gap)
-{
-    // Shrink gap by Shrink factor
-    gap = (gap*10)/13;
- 
-    if (gap < 1)
-        return 1;
-    return gap;
-}
  
 // Function to sort a[0..n-1] using Comb Sort
 void combSort(int a[], int n)
@@ -148,7 +149,7 @@ void combSort(int a[], int n)
     while (gap != 1 || swapped == true)
     {
         // Find next gap
-        gap = getNextGap(gap);
+        gap = newgap(gap);
  
         // Initialize swapped as false so that we can
         // check if swap happened or not
@@ -159,10 +160,10 @@ void combSort(int a[], int n)
         {
             if (a[i] > a[i+gap])
             {
-	      int temp = a[i];
-	      a[i] = a[i+gap];
-	      a[i+gap] = temp; 
-              swapped = true;
+				int temp = a[i];
+				a[i] = a[i+gap];
+				a[i+gap] = temp; 
+				swapped = true;
             }
         }
     }
@@ -233,26 +234,13 @@ void bubbleSort_Par(int arr[], int n, int threads)
 	}
 }
 
-int newgap(int gap)
-{
-    gap = (gap * 10) / 13;
-    /*if (gap == 9 || gap == 10)
-        gap = 11;*/
-    if (gap < 1)
-        gap = 1;
-    return gap;
-}
-
-
 void combSort_Par(int array[], int n, int threads)
 {
-    int gap = n;
-    int temp, i, j, m, h;
-    bool swapped = 1;
-	#pragma omp parallel shared(array, n) private(temp, j, m, i, swapped, gap) num_threads(threads)
-	//i think i need to add private(temp, j, m, n)
-	//swapped is shared? gap is shared? make i shared?
-	for(;;)//basically while(true)
+    int gap;
+	int temp, i, j, m, h, g, o;
+    bool swapped;
+	#pragma omp parallel shared(array, n, gap) private(o, temp, j, m, i, h, g, swapped) num_threads(threads)
+	for(o = 0;o=1;)//basically while(true)
 	{
 		gap = newgap(gap);
 		swapped = 0;
@@ -260,49 +248,39 @@ void combSort_Par(int array[], int n, int threads)
 		{
 			if(i%2 == 0)
 			{
+				
 				#pragma omp for
-				for(m = 0; m < (n-gap)/2; m++)
+				for(m = 0; m <= (n-gap); m++)
 				{
-					j = m + gap;
-					if(array[m] > array[j])
-					//if(array[2*m] > array[j*2])
+					g = m + gap;
+					if(array[m] > array[g])
 					{
 						temp = array[m];
-						array[m] = array[j];
-						array[j] = temp;	
+						array[m] = array[g];
+						array[g] = temp;	
 						swapped = 1;
-						/*
-						temp = array[2*m];
-						array[2*m] = array[2*j];
-						array[2*j] = temp;
-						*/
 					}
 				}
 			}
 			else
 			{
 				#pragma omp for
-				for(h = 0; h < ((n-gap)/2)-1; h++)
+				for(h = 0; h <= (n-gap); h++)
 				{
 					j = h + gap;
 					if(array[h] > array[j])
-					//if(array[2*h] > array[2*j])
 					{
 						temp = array[h];
 						array[h] = array[j];
 						array[j] = temp;	
 						swapped = 1;
-						/*
-						temp = array[2*h];
-						array[2*h] = array[2*j];
-						array[2*j] = temp;
-						*/
 					}
 				}
 			}
 		}
 		if(gap == 1 && !swapped)
 		{
+			o=1;
 			break;
 		}
 	}
@@ -311,91 +289,112 @@ void combSort_Par(int array[], int n, int threads)
 void shellSort_Par(int a[], int n, int threads)
 {
     int i, m;
-    #pragma omp parallel shared(a,m,n) private(i) num_threads(threads)
+    #pragma omp parallel shared(a,n) private(i, m) num_threads(threads)
     for(m = n/2; m > 0; m /= 2)
     {
-            #pragma omp for
-            for(i = 0; i < m; i++)
-                insertionsort(&(a[i]), n-i, m);
+        #pragma omp for
+        for(i = 0; i < m; i++)
+            insertionsort(&(a[i]), n-i, m);
     }
 }
-
-
-
-
-
+/*
 void parSortRun(int* lenArrs, int lenArrsLen,
-		void (*sortFun)(int*, int, int),
-		int* (*genFun)(int) ){
-  duration = 0;
-  durationAvg = 0; 
-    for(int i = 0; i < lenArrsLen; i++){
-       for(int k = 0; k < ITERATE; k++){
-	 //loop for each Len in LEN_CONST
-	 //create a new array, populate array
-	 int* tbs = genFun(lenArrs[i]); 
-	 // print_array(lenArrs[i],tbs); 
-	 start = clock(); 
-	 sortFun(tbs, lenArrs[i],NUMTHREADS);
-	 end = clock(); 
-	 durationAvg += ( end - start ) / (double) CLOCKS_PER_SEC;
-	 free(tbs);
-      //print_array(lenArrs[i],tbs);
+	void (*sortFun)(int*, int, int),
+	int* (*genFun)(int) ){
+	duration = 0;
+	durationAvg = 0; 
+    for(int i = 0; i < lenArrsLen; i++)
+	{
+		for(int k = 0; k < ITERATE; k++)
+		{
+			
+			//loop for each Len in LEN_CONST
+			//create a new array, populate array
+			int* tbs = genFun(lenArrs[i]); 
+			// print_array(lenArrs[i],tbs); 
+			start = clock(); 
+			sortFun(tbs, lenArrs[i], NUMTHREADS);
+			end = clock(); 
+			durationAvg += ( end - start ) / (double) CLOCKS_PER_SEC;
+			free(tbs);
+			//print_array(lenArrs[i],tbs);
        }
        duration = durationAvg/ITERATE;
        std::cout<<"time: "<< duration<<" length: " << lenArrs[i]<<'\n';
     }
     
 }
+*/
+void parSortRun(int* lenArrs, int lenArrsLen, void (*sortFun)(int*, int, int), int* (*genFun)(int) )
+{
+	int* tbs;
+	int a, b;
+	duration = 0;
+	durationAvg = 0; 
+	for(a = 0; a < lenArrsLen; )
+	{
+		for(b = 1; b <= ITERATE; )
+		{
+			tbs = genFun(lenArrs[a]);  
+			start = clock(); 
+			sortFun(tbs, lenArrs[a], NUMTHREADS);
+			end = clock(); 
+			durationAvg += ( end - start ) / (double) CLOCKS_PER_SEC;
+			
+			b++;
+		}
+		duration = durationAvg/ITERATE;
+		std::cout<<"time: "<< duration<<" length: " << lenArrs[a]<<'\n';
+		a++;
+    }
+	free(tbs);
+    
+}
 
+void test_correctness(int n, int array[]) {
+  int i;
+  for (i = 1; i < n; i++) {
+    if (array[i] < array[i - 1]) {
+      printf("Correctness test found error at %d: %.4f is not < %.4f but appears before it\n", i, array[i - 1], array[i]);
+    }
+  }
+}
+void init_random_vector(int n, int array[]) {
+  int i;
+  for (i = 0; i < n; i++) {
+    array[i] = (rand() % 100);
+  }
+}
  
 int main(){
-  /*
-  printf("Running BubbleSort Tests\n");
-  printf("BubbleSort Random.\n"); 
-  sortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort, generateRandomArray);
-  printf("BubbleSort Sorted\n");
-  sortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort, generateSortedArray);
-  printf("BubbleSort Sorted\n"); 
-  sortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort, generateBackwardsArray);
-  printf("Running ShellSort Tests\n");
-  printf("ShellSort Random.\n"); 
-  sortRun(LEN_CONST, LEN_CONST_LEN,shellSort, generateRandomArray);
-  printf("ShellSort Sorted:\n");
-  sortRun(LEN_CONST, LEN_CONST_LEN, shellSort, generateSortedArray);
-  printf("ShellSort Backwards:\n");
-  sortRun(LEN_CONST, LEN_CONST_LEN, shellSort, generateBackwardsArray);
-  printf("Running Combsort Tests\n");
-  printf("combSort Random:\n"); 
-  sortRun(LEN_CONST, LEN_CONST_LEN, combSort, generateRandomArray);
-  printf("combSort Sorted:\n");
-  sortRun(LEN_CONST, LEN_CONST_LEN, combSort, generateSortedArray);
-  printf("combSort Backwards: \n");
-  sortRun(LEN_CONST, LEN_CONST_LEN, combSort, generateBackwardsArray);
-  printf("Done tests\n");
-  */ 
-
-  printf("Running Parallel BubbleSort Tests\n");
-  printf("Parallel BubbleSort Random.\n"); 
-  parSortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort_Par, generateRandomArray);
-  printf("ParallelBubbleSort Sorted\n");
-  parSortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort_Par, generateSortedArray);
-  printf("Parallel BubbleSort Sorted\n"); 
-  parSortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort_Par, generateBackwardsArray);
-  printf("Running Parallel ShellSort Tests\n");
-  printf("Parallel ShellSort Random.\n"); 
-  parSortRun(LEN_CONST, LEN_CONST_LEN,shellSort_Par, generateRandomArray);
-  printf("Parallel ShellSort Sorted:\n");
-  parSortRun(LEN_CONST, LEN_CONST_LEN, shellSort_Par, generateSortedArray);
-  printf("Parallel ShellSort Backwards:\n");
-  parSortRun(LEN_CONST, LEN_CONST_LEN, shellSort_Par, generateBackwardsArray);
-  printf("ParallelRunning Combsort Tests\n");
-  printf("Parallel CombSort Random:\n"); 
-  parSortRun(LEN_CONST, LEN_CONST_LEN, combSort_Par, generateRandomArray);
-  printf("Parallel CombSort Sorted:\n");
-  parSortRun(LEN_CONST, LEN_CONST_LEN, combSort_Par, generateSortedArray);
-  printf("Parallel CombSort Backwards: \n");
-  parSortRun(LEN_CONST, LEN_CONST_LEN, combSort_Par, generateBackwardsArray);
-  return 0; 
-
+	printf("Running Parallel BubbleSort Tests\n");
+	printf("Parallel BubbleSort Random.\n"); 
+	parSortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort_Par, generateRandomArray);
+	printf("ParallelBubbleSort Sorted\n");
+	parSortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort_Par, generateSortedArray);
+	printf("Parallel BubbleSort Backwards\n"); 
+	parSortRun(LEN_CONST, LEN_CONST_LEN, bubbleSort_Par, generateBackwardsArray);
+	printf("Running Parallel ShellSort Tests\n");
+	printf("Parallel ShellSort Random.\n"); 
+	parSortRun(LEN_CONST, LEN_CONST_LEN, shellSort_Par, generateRandomArray);
+	printf("Parallel ShellSort Sorted:\n");
+	parSortRun(LEN_CONST, LEN_CONST_LEN, shellSort_Par, generateSortedArray);
+	printf("Parallel ShellSort Backwards:\n");
+	parSortRun(LEN_CONST, LEN_CONST_LEN, shellSort_Par, generateBackwardsArray);
+	/*
+	THERE IS A BUG WHERE IT DOES NOT RUN ONE COMBSORT AFTER ANOTHER AND WORKS WHEN
+	USING CIN.GET() AND HAVING USER HIT ENTER
+	Side Note: CombSort will run worse because of extra loops for parallelization?
+	*/
+	printf("ParallelRunning Combsort Tests\n");
+	printf("Parallel CombSort Random:\n"); 
+	parSortRun(LEN_CONST, LEN_CONST_LEN, combSort_Par, generateRandomArray);
+	printf("Parallel CombSort Sorted (HIT ENTER):\n");
+	std::cin.get();
+	parSortRun(LEN_CONST, LEN_CONST_LEN, combSort_Par, generateSortedArray);
+	printf("Parallel CombSort Backwards (HIT ENTER): \n");
+	std::cin.get();
+	parSortRun(LEN_CONST, LEN_CONST_LEN, combSort_Par, generateBackwardsArray);
+	
+	return 0; 
 }
